307da4f991001c7f71dfe8b44d108f57f66ae5ab
75396 <akosnik1@stu.vistula.edu.pl>
2026-05-03 12:39:56 +0200

    Fix 75396

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function zmianaStylu() {
   styl = document.getElementById("stylStrony");
 
-  if (stylesheet.getAttribute("href") === "red.css") {
+  if (styl.getAttribute("href") === "red.css") {
     styl.setAttribute("href", "green.css");
   } else {
     styl.setAttribute("href", "red.css");
